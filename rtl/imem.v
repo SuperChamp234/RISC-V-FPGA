a109ef7097fdc2bff6cdafc896d2ae8bb52f6362
SuperChamp234 <leonardh@tuta.io>
2022-08-31 12:18:34 +0530

    Instruction memory fully working

--- rtl/imem.v
+++ rtl/imem.v
@@ -1,28 +1,27 @@
 module imem(
+	input clk,
 	input [4:0] address,
 	input [31:0] data_in,
-	input rd,
-	input wr,
+	input rw,
 	output [31:0] data_out
 	//output data_valid
 );
 	reg [31:0] ins_mem [15:0];
+	reg [31:0] data_out_reg;
 	initial begin
 		ins_mem[0] <= 32'b00000000000000000000011100010011; //ADDI x14,  x0, 0
 		ins_mem[1] <= 32'b00000000101000000000011000010011; //ADDI x12,  x0, 1010
 		ins_mem[2] <= 32'b00000000111001101000011100110011; //ADD  x14, x13, x14
 		ins_mem[3] <= 32'b00000000000101101000011010010011; //ADDI x13, x13  1
 	end
-//  always @ (rd or wr) begin
-//	if(rd) begin
-//		data_out_reg <= ins_mem[address];
-//	end
-//	else if(wr) begin
-//      ins_mem[address] <= data_in;
-//	end
-//  end
-//  assign data_out = data_out_reg;
-	assign data_out = rd ? ins_mem[address] : 0;
-	//reg [31:0] ins_temp = ins_mem[address];
-	//ins_mem[address] = wr ? data_in : ins_temp;
+  always @ (posedge clk) begin
+	if(~rw) begin
+      ins_mem[address] <= data_in;
+		data_out_reg = 0;
+	end
+	else if(rw) begin
+		data_out_reg = ins_mem[address];
+	end
+  end
+  assign data_out = data_out_reg;
 endmodule
--- testbench/imem_testbench.v
+++ testbench/imem_testbench.v
@@ -2,13 +2,22 @@ module imem_testbench;
 
   wire [31:0] instr;
   reg [4:0] addr;
-  imem imem1(.address(addr), .rd(1), .data_out(instr), .wr(0), .data_in(0));
+  reg rw;
+  reg [31:0] data_in;
+  reg clk;
+  
+  imem imem1(.clk(clk), .address(addr), .rw(rw), .data_out(instr), .data_in(data_in));
 	
+	initial clk = 0;
+	always #10 clk = ~clk;
 	initial begin
+		rw = 1; 
 		addr = 5'd0; #100;
 		addr = 5'd1; #100;
 		addr = 5'd2; #100;
-		addr = 5'd3; #100;
+		addr = 5'd1; rw=0; data_in = 32'b00000000000101101000011010010011; #100;
+      rw = 1;
+		addr = 5'd1; #100;
 	end
 	
 endmodule
