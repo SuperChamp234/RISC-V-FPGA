356cbbc83088d416e90e265330173e7f8b68ee84
Harsh00788 <104072702+Harsh00788@users.noreply.github.com>
2022-09-16 17:01:54 +0530

    Update ALU.v

--- rtl/ALU.v
+++ rtl/ALU.v
@@ -1,16 +1,17 @@
-module ALU (
+module ALU(
 input [31:0] rs1,
-input [31:0} rs2,
+input [31:0] rs2,
 input imm_valid,
-input [36:0} instr_bus,
+input [31:0] imm,
+input [36:0] instr,
 input [31:0] extend,
 input [3:0] ctrl,
+input [31:0] PC,
 output reg zero,
-output reg ALUoutput
-
+output reg [31:0] ALUoutput
 
 );
-
+always@(*)
 if(instr[0]==1)
    begin
 	ALUoutput<=rs1+rs2;
@@ -45,21 +46,13 @@ else if(instr[6]==1)
      begin
 	  ALUoutput <= rs1 >> rs2;
 	  end
-	  
-else if(instr[7]==1)
-     begin
-	  ALUoutput <= rs1
-	  end
+	 
 	  
 else if(instr[8]==1)
      begin
 	  ALUoutput <= (rs1 < rs2)?1:0;
 	  end
 
-else if(instr[9]==1)
-     begin
-	  ALUoutput <= 
-	  end
 
 else if(instr[10]==1)
      begin
@@ -81,9 +74,83 @@ else if(instr[13]==1)
 	  ALUoutput <= rs1 & imm;
      end
 
-else if(instr[14]==1)
+else if(instr[16]==1)
      begin
-	  ALUoutput <= 
+	  ALUoutput <= (rs1 < imm)?1:0;
+	  end
+
+else if(instr[17]==1)
+     begin 
+	  ALUoutput <= rs1+imm;
+	  
+	  end
+	  
+else if(instr[27]==1)
+     begin
+	  if
+	  (rs1==rs2)
+	  ALUoutput <= PC + imm;
+	  end
 	 
+	  
+	  
+else if(instr[28]==1)
+     begin
+	  if
+	  (rs1!=rs2)
+	  ALUoutput <= PC + imm;
+	  end
+	
+	  
+else if(instr[29]==1)
+     begin
+	  if(rs1<rs2)
+	  ALUoutput <= PC + imm;
+	  
+	  end
+
+	  
+else if(instr[30]==1)
+     begin
+	  if(rs1==rs2)
+	  ALUoutput <= PC + imm;
+	  end
+	  
+	  
+else if(instr[31]==1)
+     begin
+	  if(rs1==rs2)
+	  ALUoutput <= PC + imm;
+	  end
+	  
+	  
+else if(instr[32]==1)
+     begin
+	  if(rs1>=rs2)
+	  ALUoutput <= PC + imm;
+	  end
+	  
+	  
+else if(instr[33]==1)
+     begin
+	  if(rs1<rs2)
+	  ALUoutput <= PC + imm;
+	  end
+
+	  
+else if(instr[34]==1)
+     begin
+	  ALUoutput <= PC + 4;
+	  end
+	  
+else if(instr[35]==1)
+     begin
+	  ALUoutput <= PC + 4;
+	  end
+	 
+
+endmodule
+
+
     
     
