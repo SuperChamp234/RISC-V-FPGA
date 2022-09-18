94ff5c92a911e0d11a977c1bed77c1764afc2df4
SuperChamp234 <leonardh@tuta.io>
2022-09-19 00:59:59 +0530

    Reworked BCD Algorithm

--- rtl/binary_to_bcd.v
+++ rtl/binary_to_bcd.v
@@ -3,38 +3,66 @@ module binary_to_bcd(
     output reg [39:0] bcd
 );
 
+function [31:0] div_by_10(
+    input[31:0] n,
+    input[31:0] q, //00
+    input[31:0] r //00
+); 
+    
+    begin
+        q = (n >> 1) + (n >> 2);
+        q = q + (q >> 4);
+        q = q + (q >> 8);
+        q = q + (q >> 16);
+        q = q >> 3;
+        r = n - (((q << 2) + q) << 1);
+        div_by_10 = q + (r > 9);
+    end
+endfunction
+
 reg [31:0] inp;
+reg [31:0] next;
 
 always @(*) begin
     inp = bin;
 
-    bcd[3:0] = inp % 10; //1
-    inp = inp/10;
+    next = div_by_10(inp,32'b0,32'b0);
+    bcd[3:0] = inp - ((next << 3) + (next << 1));//inp - ((next << 3) + (next << 1)); //1
+    inp = next;
 
-    bcd[7:4] =  inp % 10; //2
-    inp = inp/10;
+    next = div_by_10(inp,32'b0,32'b0);
+    bcd[7:4] =  inp - ((next << 3) + (next << 1)); //2
+    inp = next;
 
-    bcd[11:8] =  inp % 10; //3
-    inp = inp/10;
+    next = div_by_10(inp,32'b0,32'b0);
+    bcd[11:8] =  inp - ((next << 3) + (next << 1)); //3
+    inp = next;
 
-    bcd[15:12] =  inp % 10; //4
-    inp = inp/10;
+    next = div_by_10(inp,32'b0,32'b0);
+    bcd[15:12] =  inp - ((next << 3) + (next << 1)); //4
+    inp = next;
 
-    bcd[19:16] =  inp % 10; //5
-    inp = inp/10;
+    next = div_by_10(inp,32'b0,32'b0);
+    bcd[19:16] =  inp - ((next << 3) + (next << 1)); //5
+    inp = next;
 
-    bcd[23:20] =  inp % 10; //6
-    inp = inp/10;
+    next = div_by_10(inp,32'b0,32'b0);
+    bcd[23:20] =  inp - ((next << 3) + (next << 1)); //6
+    inp = next;
 
-    bcd[27:24] =  inp % 10; //7
-    inp = inp/10;
+    next = div_by_10(inp,32'b0,32'b0);
+    bcd[27:24] =  inp - ((next << 3) + (next << 1)); //7
+    inp = next;
 
-    bcd[31:28] =  inp % 10; //8
-    inp = inp/10;
+    next = div_by_10(inp,32'b0,32'b0);
+    bcd[31:28] =  inp - ((next << 3) + (next << 1)); //8
+    inp = next;
 
-    bcd[35:32] =  inp % 10; //9
-    inp = inp/10;
+    next = div_by_10(inp,32'b0,32'b0);
+    bcd[35:32] =  inp - ((next << 3) + (next << 1)); //9
+    inp = next;
 
-    bcd[39:36] =  inp % 10; //10
+    next = div_by_10(inp,32'b0,32'b0);
+    bcd[39:36] =  inp - ((next << 3) + (next << 1)); //10
 end
 endmodule
--- testbench/binary_to_bcd_tb.v
+++ testbench/binary_to_bcd_tb.v
@@ -6,7 +6,7 @@ wire [39:0] bcd;
 binary_to_bcd bin_bcd1(.bin(bin), .bcd(bcd));
 
 initial begin
-    bin = 32'd36886;#20;
+    bin = 32'd12345;#20;
     $stop;
 end
 
